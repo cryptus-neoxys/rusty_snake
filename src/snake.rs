eadde84f2bcbac7a11899a5308607517f3fd4479
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-10-19 01:42:31 +0530

    ✨ done 🐍 logic

--- src/snake.rs
+++ src/snake.rs
@@ -6,6 +6,7 @@ use crate::draw::draw_block;
 
 const SNAKE_COLOR: Color = [188.0 / 255.0, 147.0 / 255.0, 26.0 / 255.0, 1.0];
 
+#[derive(Copy, Clone, PartialEq)]
 pub enum Direction {
     Up,
     Down,
@@ -24,6 +25,7 @@ impl Direction {
     }
 }
 
+#[derive(Debug, Clone)]
 struct Block {
     x: i32,
     y: i32,
@@ -52,5 +54,81 @@ impl Snake {
         for block in &self.body {
             draw_block(SNAKE_COLOR, block.x, block.y, con, g);
         }
+    }
+
+    pub fn head_position(&self) -> (i32, i32) {
+        let head_block = self.body.front().unwrap();
+        (head_block.x, head_block.y)
+    }
+
+    pub fn move_forward(&mut self, dir: Option<Direction>) {
+        if let Some(d) = dir {
+            self.direction = d
+        }
+
+        let (last_x, last_y): (i32, i32) = self.head_position();
+
+        let new_block = match self.direction {
+            Direction::Up => Block {
+                x: last_x,
+                y: last_y - 1,
+            },
+            Direction::Down => Block {
+                x: last_x,
+                y: last_y + 1,
+            },
+            Direction::Left => Block {
+                x: last_x - 1,
+                y: last_y,
+            },
+            Direction::Right => Block {
+                x: last_x + 1,
+                y: last_y,
+            },
+        };
+
+        self.body.push_front(new_block);
+        let removed_block = self.body.pop_back().unwrap();
+        self.tail = Some(removed_block);
+    }
+
+    pub fn head_direction(&self) -> Direction {
+        self.direction
+    }
+
+    pub fn next_head(&self, dir: Option<Direction>) -> (i32, i32) {
+        let (head_x, head_y): (i32, i32) = self.head_position();
+
+        let mut moving_dir = self.direction;
+        if let Some(d) = dir {
+            moving_dir = d
+        };
+
+        match moving_dir {
+            Direction::Up => (head_x, head_y - 1),
+            Direction::Down => (head_x, head_y + 1),
+            Direction::Left => (head_x - 1, head_y),
+            Direction::Right => (head_x + 1, head_y),
+        }
+    }
+
+    pub fn restore_tail(&mut self) {
+        let blk = self.tail.clone().unwrap();
+        self.body.push_back(blk);
+    }
+
+    pub fn overlap_tail(&self, x: i32, y: i32) -> bool {
+        let mut ch = 0;
+        for block in &self.body {
+            if x == block.x && y == block.y {
+                return true;
+            }
+
+            ch += 1;
+            if ch == self.body.len() - 1 {
+                break;
+            }
+        }
+        return false;
     }
 }
