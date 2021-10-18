faf3e389ac8b494e9f4ae92fba99a77d10dd9bcd
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-10-19 01:16:57 +0530

    ✨ begin 🐍 logic

---
+++ src/snake.rs
@@ -0,0 +1,56 @@
+use piston_window::types::Color;
+use piston_window::{Context, G2d};
+use std::collections::LinkedList;
+
+use crate::draw::draw_block;
+
+const SNAKE_COLOR: Color = [188.0 / 255.0, 147.0 / 255.0, 26.0 / 255.0, 1.0];
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match *self {
+            Direction::Down => Direction::Up,
+            Direction::Up => Direction::Down,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
+
+struct Block {
+    x: i32,
+    y: i32,
+}
+
+pub struct Snake {
+    direction: Direction,
+    body: LinkedList<Block>,
+    tail: Option<Block>,
+}
+
+impl Snake {
+    pub fn new(x: i32, y: i32) -> Snake {
+        let mut body: LinkedList<Block> = LinkedList::new();
+        body.push_back(Block { x: x + 2, y });
+        body.push_back(Block { x: x + 1, y });
+        body.push_back(Block { x, y });
+
+        Snake {
+            direction: Direction::Right,
+            body,
+            tail: None,
+        }
+    }
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        for block in &self.body {
+            draw_block(SNAKE_COLOR, block.x, block.y, con, g);
+        }
+    }
+}
